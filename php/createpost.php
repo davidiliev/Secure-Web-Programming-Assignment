07e75834e002802c9adbecc84a78c79e072e1c8b
lochieif <48900588+lochieif@users.noreply.github.com>
2022-05-14 15:51:01 +1000

    Create.html can now post to DB

---
+++ php/createpost.php
@@ -0,0 +1,18 @@
+<?php 
+
+include 'dbconn.php';
+$userName = "testUsername";
+$title = htmlspecialchars($_POST['title']);
+$date = htmlspecialchars($_POST['dateName']);
+$content = htmlspecialchars($_POST['content']);
+$sql = "INSERT INTO Post(username, title, date, content)
+VALUES('$userName', '$title','$date','$content')";
+  if (mysqli_query($conn, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+
+mysqli_close($conn);
+header("Location: index.html");
+?>
