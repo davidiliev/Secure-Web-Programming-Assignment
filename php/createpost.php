d9e14d9b57c38647be1ac14eab76e0734c836923
davidiliev <46295455+davidiliev@users.noreply.github.com>
2022-05-20 10:30:28 +1000

    Prevent sql error from appearing in browser

--- php/createpost.php
+++ php/createpost.php
@@ -2,18 +2,22 @@
 session_start();
 include 'dbconn.php';
 
-$userName = htmlspecialchars($_SESSION['username']);
-$title = htmlspecialchars($_POST['title']);
-$date = htmlspecialchars($_POST['dateName']);
-$content = htmlspecialchars($_POST['content']);
-$sql = "INSERT INTO Post(username, title, date, content)
-VALUES('$userName', '$title','$date','$content')";
-  if (mysqli_query($conn, $sql)) {
-        echo "New record has been added successfully !";
-     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($conn);
-     }
-
-mysqli_close($conn);
-header('Location: ../index.php');
+if (isset($_SESSION['username'])) {
+   $userName = htmlspecialchars($_SESSION['username']);
+   $title = htmlspecialchars($_POST['title']);
+   $date = htmlspecialchars($_POST['dateName']);
+   $content = htmlspecialchars($_POST['content']);
+   $sql = "INSERT INTO Post(username, title, date, content)
+   VALUES('$userName', '$title','$date','$content')";
+     if (mysqli_query($conn, $sql)) {
+           echo "New record has been added successfully !";
+        } else {
+           echo "Error: " . $sql . ":-" . mysqli_error($conn);
+        }
+   mysqli_close($conn);
+   header('Location: ../index.php');
+} else {
+   mysqli_close($conn);
+   echo "<h1>Permission denied.</h1>";
+}
 ?>
